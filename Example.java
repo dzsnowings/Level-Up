16614b465826c54bae3a2b62f36fbea712477ddb
Danielle Zhao <dzsnowings@gmail.com>
2019-09-09 19:25:06 -0400

    Added Other.java

--- Example.java
+++ Example.java
@@ -1,6 +1,7 @@
 public class Example {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Bye.");
+        Other.say();
+        System.out.println("Bye!");
     }
 }
